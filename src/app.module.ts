f5456d4cd5bdf49144aa473c3eae64fbadb30114
tcerdaITBA <tcerda@itba.edu.ar>
2020-02-15 21:28:26 +0100

    Add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { AuthorModule } from './authors/author.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       definitions: {
